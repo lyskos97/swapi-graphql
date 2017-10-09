f2dd2fd59ec513cb71662947a2db9123c6536c14
lyskos97 <lyskos97@gmail.com>
2017-10-09 22:09:08 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+app.all('/graphql', (req, res) => res.redirect('/'));
+
+app.use('/', expressGraphQL({
+  schema: schema,
+  graphiql: true,
+}));
+
+app.listen(4000, () => {
+  console.log('Everything works on port 4000...');
+});
