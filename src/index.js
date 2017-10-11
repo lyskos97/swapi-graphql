d83bb82b5cdcb6c6369feb5538a319e5bdaab44a
lyskos97 <lyskos97@gmail.com>
2017-10-12 01:50:03 +0600

    "Get all" queries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import expressGraphQL from 'express-graphql';
 import schema from './schema';
+// import { loadData } from './schema/utils';
 
 const app = express();
 
@@ -12,6 +13,18 @@ app.use(
   })
 );
 
-app.listen(4000, () => {
-  console.log('Everything works on port 4000...');
+app.listen(3000, () => {
+  console.log('Everything works on port 3000...');
 });
+
+/* const k = async (pages = 9) => {
+  const fullData = {};
+  for (let i = 1; i < pages + 1; i++) {
+    const data = loadData(`https://swapi.co/api/people/?page=${i}`);
+    console.log(a data);
+    Object.assign(fullData, data);
+  }
+  console.log(fullData, '+ kek');
+};
+
+k(); */
--- src/schema/Query.js
+++ src/schema/Query.js
@@ -3,6 +3,9 @@ import { loadData } from './utils';
 import PersonType from './Person';
 import PlanetType from './Planet';
 import FilmType from './Film';
+import SpeciesType from './Species';
+import StartshipType from './Starship';
+import VehicleType from './Vehicle';
 
 const QueryType = new GraphQLObjectType({
   name: 'Query',
@@ -18,11 +21,12 @@ const QueryType = new GraphQLObjectType({
       type: new GraphQLList(PersonType),
       args: {
         limit: { type: GraphQLInt, defaultValue: 5 },
+        offset: { type: GraphQLInt, defaultValue: 0 },
       },
       resolve: async (source, args) => {
         const data = await loadData(`https://swapi.co/api/people/`);
-        if (args && args.limit > 0) {
-          return data.slice(0, args.limit);
+        if (args && args.limit > 0 && args.offset >= 0) {
+          return data.slice(args.offset, args.limit + args.offset);
         }
         return data;
       },
@@ -47,6 +51,45 @@ const QueryType = new GraphQLObjectType({
       },
       resolve: async (_, args) => {
         const data = await loadData(`https://swapi.co/api/films/`);
+        if (args && args.limit > 0) {
+          return data.slice(0, args.limit);
+        }
+        return data;
+      },
+    },
+    species: {
+      type: new GraphQLList(SpeciesType),
+      args: {
+        limit: { type: GraphQLInt, defaultValue: 3 },
+      },
+      resolve: async (_, args) => {
+        const data = await loadData(`https://swapi.co/api/species/`);
+        if (args && args.limit > 0) {
+          return data.slice(0, args.limit);
+        }
+        return data;
+      },
+    },
+    starships: {
+      type: new GraphQLList(StartshipType),
+      args: {
+        limit: { type: GraphQLInt, defaultValue: 3 },
+      },
+      resolve: async (_, args) => {
+        const data = await loadData(`https://swapi.co/api/starships/`);
+        if (args && args.limit > 0) {
+          return data.slice(0, args.limit);
+        }
+        return data;
+      },
+    },
+    vehicles: {
+      type: new GraphQLList(VehicleType),
+      args: {
+        limit: { type: GraphQLInt, defaultValue: 3 },
+      },
+      resolve: async (_, args) => {
+        const data = await loadData(`https://swapi.co/api/vehicles/`);
         if (args && args.limit > 0) {
           return data.slice(0, args.limit);
         }
